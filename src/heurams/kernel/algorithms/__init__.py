fa2f8fa7018402e953368b632deacf1da2e92ab8
Wang Zhiyu <pluvium27@outlook.com>
2026-04-25 01:54:18 +0800

    feat: 增加早期 FSRS 支持

--- src/heurams/kernel/algorithms/__init__.py
+++ src/heurams/kernel/algorithms/__init__.py
@@ -2,17 +2,20 @@ from .base import BaseAlgorithm
 from .sm2 import SM2Algorithm
 from .sm15m import SM15MAlgorithm
 from .nsp0 import NSP0Algorithm
+from .fsrs import FSRSAlgorithm
 
 __all__ = [
     "SM2Algorithm",
     "BaseAlgorithm",
     "SM15MAlgorithm",
     "NSP0Algorithm",
+    "FSRSAlgorithm",
 ]
 
 algorithms = {
     "SM-2": SM2Algorithm,
     "NSP-0": NSP0Algorithm,
     "SM-15M": SM15MAlgorithm,
+    "FSRS": FSRSAlgorithm,
     "Base": BaseAlgorithm,
 }
